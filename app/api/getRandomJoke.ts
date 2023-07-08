95a89c539b589506a4a6f65bb50a948da0d1995a
Gauthierboddaert <Boddaert.gauthier@gmail.com>
2023-07-08 19:37:17 +0200

    fix: disallow bad joke

--- app/api/getRandomJoke.ts
+++ app/api/getRandomJoke.ts
@@ -6,7 +6,12 @@ const getRandomJoke = async (): Promise<JokeResponse> => {
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjEyNjcyOTAwNDMwNDMwMjExIiwibGltaXQiOjEwMCwia2V5IjoidzZzejcxdVNqZG5QbnhKampFdHE4U3NrVzQ0b1hsNlVJeWVUUjhlOXFBMUVvb21DQ1oiLCJjcmVhdGVkX2F0IjoiMjAyMy0wNy0wOFQxNTo1MDozMSswMDowMCIsImlhdCI6MTY4ODgzMTQzMX0.7KSJAZ5jFHsywoKEeSKs9fexLDco434q2D524H2M3-w"
   );
   
-  return await blague.random();
+  return await blague.random({
+    disallow: [
+      blague.categories.GLOBAL,
+      blague.categories.BLONDES,
+    ]
+  });
 };
 
 export default getRandomJoke;
